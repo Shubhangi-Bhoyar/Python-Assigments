0e6f18d65154fcb3541ca5e9270620f306140d82
ShubhangiKGBhoyar <shubhbhoyar25@gmail.com>
2024-05-08 11:27:24 +0530

    changes in numerology file

--- numerology1.py
+++ numerology1.py
@@ -1,11 +1,11 @@
-n=input("enter your date of birth : ",)
+n=input("enter your date of birth(1-31) : ",)
 def fun(n):
-  if len(n) == 1 :
+   if len(n) == 1 :
       return int(n)
-  total = 0
-  for i in n :
-      total += int(i)
-  return fun(str(total))
+   total = 0
+   for i in n :
+       total += int(i)
+   return fun(str(total))
 
 print("your life path number is ",fun(n))  
 
