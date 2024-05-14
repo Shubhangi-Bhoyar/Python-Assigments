3b8420f1eeeddfcc39e2694a080cdd549d792f12
ShubhangiKGBhoyar <167519021+ShubhangiKGBhoyar@users.noreply.github.com>
2024-05-14 19:22:27 +0530

    Update constructor.py

--- constructor.py
+++ constructor.py
@@ -2,15 +2,19 @@ class vehicle():
 
     def __init__(self,engine):
         self.engine = engine
-        print("engine is of ")
+        #print("engine details ")
 
 class car(vehicle):
     def __init__(self,engine,maxspeed):
         super().__init__(engine)
-        self.maxspee = maxspeed
-        print("max speed is")
-
-jaguar = car('5000 cc','217kmph')
-print('engine={car.engine},speed={car.maxspeed}')
+        self.maxspeed = maxspeed
+        #print("max speed details")
+class car2(car):
+    def __init__(self,engine,maxspeed,mileage):
+        super().__init__(engine,maxspeed)
+        self.mileage = mileage
+        
+jaguar = car2('5000 cc','217kmph','14.49kmpl')
+print('engine={jaguar.engine},speed={jaguar.maxspeed},mileage={jaguar.mileage}')
 
 
